e3059a6d9fc7af06080a05f1dd308af0fc7f58c8
Oleh Zavhorodnii <oleggenius95@gmail.com>
2020-04-08 19:04:18 +0300

    Updates

---
+++ precache-manifest.ef6cbcd08882d9fe4a5014fa6932d15b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7fb5f3e2b00eff5dd246cf51be946c0",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "b97de74c53046be163a1",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.6cc9051b.chunk.css"
+  },
+  {
+    "revision": "c1986c37cf9e40ba978d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.3b1bf193.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.3b1bf193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b97de74c53046be163a1",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.e7db455f.chunk.js"
+  },
+  {
+    "revision": "4dc4691f794833de7ba9",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.bd2cb211.js"
+  }
+]);
